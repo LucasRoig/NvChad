4fafedcb8c440138c9ca14e3fa31d2827a6c4d74
LucasRoig <lucas1.roig@gmail.com>
2023-06-20 07:05:29 +0200

    add mason config

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,11 +1,24 @@
 local plugins = {
     {
     "neovim/nvim-lspconfig",
-     config = function()
+    config = function()
         require "plugins.configs.lspconfig"
         require "custom.configs.lspconfig"
-     end,
-    }
+    end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = {
+           ensure_installed = {
+             "lua-language-server",
+             "html-lsp",
+             "prettier",
+             "stylua",
+             "typescript-language-server",
+             "prisma-language-server",
+             "dockerfile-language-server"
+           },
+         },
+       }
 }
-
 return plugins;
